36b954807a0f0ad05de8059a15309317a93e760f
O. Riastanjung <87843869+oriastanjung@users.noreply.github.com>
2021-08-18 19:59:25 +0700

    first upload
    adding existing file

---
+++ script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.list-white').click(function(){
+        $('ul').toggleClass('show');
+    });
+});
+
+
+const listWhite = document.querySelector(".list-white");
